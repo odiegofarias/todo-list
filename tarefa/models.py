09cd4461b24ef57b264efde13749cb224bb7e2ab
odiegofarias <diego.farias.fernandes@gmail.com>
2022-09-28 20:09:02 -0300

    Criada view, template e url de tarefa

--- tarefa/models.py
+++ tarefa/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Tarefa(models.Model):
+    nome = models.CharField(max_length=65)
+    feita = models.BooleanField(default=False)
+    data_criacao = models.DateTimeField(auto_now_add=True)
--- tarefa/views.py
+++ tarefa/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Olá, Mundo')
+    return render(request, 'tarefa/index.html')
